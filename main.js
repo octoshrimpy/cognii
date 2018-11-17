cffe59fab150d9a0b13c70d709a17abfc6669ccd
octoshrimpy <octavo.acapella@gmail.com>
2018-11-16 21:43:49 -0700

    page now plays different randomized audio

--- main.js
+++ main.js
@@ -1,34 +1,56 @@
 
 const audioSources = {
   'classical': {
-    'src': '',
+    'src': './classical',
     'files': [
-      '',
-      '',
-      ''
+      '0',
+      '1',
+      '2'
     ]
   },
   'hard': {
-
+    'src': './hard',
+    'files': [
+      '0',
+      '1',
+      '2'
+    ]
   },
   'meditation': {
-
+    'src': './meditation',
+    'files': [
+      '0',
+      '1',
+      '2'
+    ]
   }
 }
 
 
 $(document).ready(function(){
 
-  let isPlaying = false;
-  const rain = new Audio('./test.mp3')
+  let currentlyPlaying = new Audio('');
 
-  $(document).click(function(){
-    if(!isPlaying){
-      rain.play()
-      isPlaying = true;
-    }else {
-      rain.pause()
-      isPlaying = false;
+  // by listening to document, we can make sure the event doesn't
+  // get triggered from the current choice. don't have to unload events.
+  $(document).on('click', '.choice:not(.is-choice)', function(){
+    $('.choice.is-choice').removeClass('is-choice')
+    $(this).addClass('is-choice')
+
+    // grab a random file from the folder and play it
+    const choice = $(this).data('choice')
+
+    const randFileLoc = audioSources[choice].src
+    const randFileArray = audioSources[choice].files
+    const randomFileName = randFileArray[Math.floor(Math.random()*randFileArray.length)];
+
+
+    // console.log(!currentlyPlaying.paused || currentlyPlaying.currentTime)
+    if(!currentlyPlaying.paused || currentlyPlaying.currentTime){
+      currentlyPlaying.pause()
     }
+
+    currentlyPlaying = new Audio(`${randFileLoc}/${randomFileName}.mp3`)
+    currentlyPlaying.play()
   })
 })
