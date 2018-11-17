888c58014641a58eb7ed8d4f97b20ab4f9fb7cf4
octoshrimpy <octavo.acapella@gmail.com>
2018-11-16 18:40:58 -0700

    Adds testing audio
    adds playing from file without needing an HTML audio element. https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // this is where all the js will be stored
+$(document).ready(function(){
+
+  let isPlaying = false;
+  
+  $(document).click(function(){
+    if(!isPlaying){
+      const rain = new Audio('./test.mp3')
+      rain.play()
+      isPlaying = true;
+    }
+  })
+})
